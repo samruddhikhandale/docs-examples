5bc1c8115b6867947e63b037b7128c06bfa68770
Samruddhi Khandale <samruddhikhandale@github.com>
2023-05-01 20:12:27 +0000

    use sudo bash

--- .devcontainer/advanced-integration/welcome-message.sh
+++ .devcontainer/advanced-integration/welcome-message.sh
@@ -26,4 +26,4 @@ if [ -n "$CLIENT_ID" ] && [ -n "$APP_SECRET" ]; then
     WELCOME_MESSAGE="${ALTERNATE_WELCOME_MESSAGE}"
 fi
 
-echo "${WELCOME_MESSAGE}" | sudo /usr/local/etc/vscode-dev-containers/first-run-notice.txt
+sudo bash -c "echo $WELCOME_MESSAGE > /usr/local/etc/vscode-dev-containers/first-run-notice.txt"
