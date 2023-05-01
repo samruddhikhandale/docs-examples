7c8c7ea3121d58d713911744265cd2455442c73b
Samruddhi Khandale <samruddhikhandale@github.com>
2023-05-01 19:06:28 +0000

    nit

--- .devcontainer/advanced-integration/welcome-message.sh
+++ .devcontainer/advanced-integration/welcome-message.sh
@@ -1,23 +1,23 @@
-!/usr/bin
+#!/usr/bin
 
-set -eux
+set -e
 
 WELCOME_MESSAGE="
 👋 Welcome to the \"Checkout Advanced Integration Example\"
 
 🛠️  Your environment is fully setup with all the required software.
 
-🚀 Once you rename the \".env.example\" file to \".env\" and update \"CLIENT_ID\" and \"APP_SECRET\", the checkout page will automatically open in the browser when the server is started."
+🚀 Once you rename the \".env.example\" file to \".env\" and update \"CLIENT_ID\" and \"APP_SECRET\", the checkout page will automatically open in the browser after the server is restarted."
 
 ALTERNATE_WELCOME_MESSAGE="
 👋 Welcome to the \"Checkout Advanced Integration Example\"
 
 🛠️  Your environment is fully setup with all the required software.
 
-🚀 The checkout page will automatically open in the browser when the server is started."
+🚀 The checkout page will automatically open in the browser after the server is started."
 
-if [[ -n \"$CLIENT_ID\" ]] && [[ -n \"$APP_SECRET\" ]]; then
+if [ -n "$CLIENT_ID" ] && [ -n "$APP_SECRET" ]; then
     WELCOME_MESSAGE="${ALTERNATE_WELCOME_MESSAGE}"
 fi
 
-sudo echo "${WELCOME_MESSAGE}" >> /usr/local/etc/vscode-dev-containers/first-run-notice.txt
+echo "${WELCOME_MESSAGE}" > /usr/local/etc/vscode-dev-containers/first-run-notice.txt
